28927d0aadaa20485c27eb51dd2008d17909611e
bsaxen <benny.saxen@gmail.com>
2018-11-19 17:12:34 +0100

    Update gowServer.php

--- gowServer.php
+++ gowServer.php
@@ -1,14 +1,13 @@
 <?php
 //=============================================
 // File.......: gowServer.php
-// Date.......: 2018-11-18
+// Date.......: 2018-11-19
 // Author.....: Benny Saxen
 // Description: Glass Of Water Server
 //=============================================
 // Configuration
 //=============================================
-//$conf_gps = 'http://gow.simuino.com/gowPlatformServer.php';
-$conf_gps = 'http://127.0.0.1/git/gow/gowPlatformServer.php';
+// No configuration
 //=============================================
 $conf_action_file_name = 'action.gow';
 //=============================================
@@ -45,8 +44,9 @@ function readActionFile($topic)
       // Delete file
       if (file_exists($action_file)) unlink($action_file);
   }
-  else {
-      $result = " ";
+  else 
+  {
+      $result = ":void:";
   }
   return $result;
 }
@@ -62,153 +62,228 @@ function writeActionFile($topic, $action)
     fwrite($file,$action);
     fclose($file);
   }
-  else {
+  else 
+  {
       $result = " ";
   }
   return $result;
 }
 //=============================================
-function registrationGPS($url,$topic,$type,$period,$hw)
+function createTopic($topic)
 //=============================================
 {
-  global $conf_gps;
-  $request = $conf_gps;
-  $request = $request."?do=register&topic=$topic&type=$type&period=$period&url=$url&hw=$hw";
-  $res = file_get_contents($request);
+
 }
+//=============================================
+function deleteTopic($topic)
+//=============================================
+{
+
+}
+//=============================================
+function findTopic($topic)
+//=============================================
+{
+
+}
+//=============================================
+function listAllTopics($topic)
+//=============================================
+{
+
+}
+//=============================================
+function searchTopics($search)
+//=============================================
+{
+
+}
+
+
 //=============================================
 // End of library
 //=============================================
 
-$error = 1;
-if (isset($_GET['topic'])) {
-  $topic = $_GET['topic'];
-  $error = 0;
-  if (!is_dir($topic)) {
-    mkdir($topic, 0777, true);
-  }
-}
-else
+if (isset($_GET['do'])) 
 {
-  $error = 2;
-  echo "error 2";
-}
+    $do = $_GET['do'];
+  
+    if ($do == 'list')
+    {
+      listAllTopics($list);
+    }
+  
+    if ($do == 'find')
+    {
+      $search = 'void';
+      $search = $_GET['search'];
+      searchTopics($search);
+    }
+  
+    // Check if topic is given
+    $error = 1;
+    if (isset($_GET['topic'])) 
+    {
+      $topic = $_GET['topic'];
+      $error = 0;
+      if (!is_dir($topic)) 
+      {
+         mkdir($topic, 0777, true);
+      }
+    }
+    else
+    {
+      $error = 2;
+      echo "error 2";
+    }
+  
+    // API when topic is available
+    if($error == 0)
+    {
+      if ($do == 'action') 
+      {
+        $order = $_GET['order'];
+        writeActionFile($topic, $order);
+      }
+      if ($do == 'delete') 
+      {
+        deleteTopic($topic);
+      }
 
-if($error == 0)
-{
-
-  if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-    writeActionFile($topic, $action);
-    exit;
-  }
 
 
-  if (isset($_GET['no'])) {
-    $no = $_GET['no'];
-  }
-  if (isset($_GET['type'])) {
-    $type = $_GET['type'];
-  }
-  if (isset($_GET['value'])) {
-    $value = $_GET['value'];
-  }
-  if (isset($_GET['unit'])) {
-    $unit = $_GET['unit'];
-  }
-  if (isset($_GET['ts'])) {
-    $ts = $_GET['ts'];
-  }
-  if (isset($_GET['period'])) {
-    $period = $_GET['period'];
-  }
-  if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-  }
-  if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-  }
-  if (isset($_GET['hw'])) {
-    $hw = $_GET['hw'];
-  }
 
-  registrationGPS($url,$topic,$type,$period,$hw);
 
-    //===========================================
-    // HTML
-    //===========================================
 
-    $fdoc = $topic.'/doc.html';
 
-    $doc = fopen($fdoc, "w");
-    fwrite($doc, "<html>");
-    fwrite($doc, "<body bgcolor=\"#9EB14A\">");
-    fwrite($doc, "TOPIC       ".$topic);
-    fwrite($doc, "<br>");
-    fwrite($doc, "NO          ".$no);
-    fwrite($doc, "<br>");
-    fwrite($doc, "TYPE        ".$type);
-    fwrite($doc, "<br>");
-    fwrite($doc, "VALUE       ".$value);
-    fwrite($doc, "<br>");
-    fwrite($doc, "UNIT        ".$unit);
-    fwrite($doc, "<br>");
-    fwrite($doc, "TS          ".$ts);
-    fwrite($doc, "<br>");
-    fwrite($doc, "PERIOD      ".$period);
-    fwrite($doc, "<br>");
-    fwrite($doc, "GS_TS       ".$gs_ts);
-    fwrite($doc, "<br>");
-    fwrite($doc, "URL         ".$url);
-    fwrite($doc, "<br>");
-    fwrite($doc, "HW          ".$hw);
-    fwrite($doc, "</body></html>");
-    fclose($doc);
 
-    //===========================================
-    // JSON
-    //===========================================
-    $fdoc = $topic.'/doc.json';
-    $doc = fopen($fdoc, "w");
-    fwrite($doc, "{\"gow\": {\n");
-      fwrite($doc, "   \"topic\":  \"$topic\",\n");
-      fwrite($doc, "   \"no\":     \"$no\",\n");
-      fwrite($doc, "   \"type\":   \"$type\",\n");
-      fwrite($doc, "   \"value\":  \"$value\",\n");
-      fwrite($doc, "   \"unit\":   \"$unit\",\n");
-      fwrite($doc, "   \"ts\":     \"$ts\",\n");
-      fwrite($doc, "   \"period\": \"$period\",\n");
-      fwrite($doc, "   \"gs_ts\":  \"$gs_ts\"\n");
-      fwrite($doc, "   \"url\":    \"$url\"\n");
-      fwrite($doc, "   \"hw\":     \"$hw\"\n");
-      fwrite($doc, "}}\n ");
-      fclose($doc);
+      if ($do == 'data') 
+      { 
+        // Default values
+        $no = 999;
+        $type = 'no_type';
+        $value = 999;
+        $ts = 'no_device_timestamp';
+        $period = 999;
+        $url = 'no_url';
+        $hw = 'no_hw';
 
-    //===========================================
-    // TXT
-    //===========================================
-    $fdoc = $topic.'/doc.txt';
-    $doc = fopen($fdoc, "w");
-    fwrite($doc,   "TOPIC        $topic\n");
-    fwrite($doc,   "NO           $no\n");
-    fwrite($doc,   "TYPE         $type\n");
-    fwrite($doc,   "VALUE        $value\n");
-    fwrite($doc,   "UNIT         $unit\n");
-    fwrite($doc,   "TS           $ts\n");
-    fwrite($doc,   "PERIOD       $period\n");
-    fwrite($doc,   "GS_TS        $gs_ts\n");
-    fwrite($doc,   "URL          $url\n");
-    fwrite($doc,   "HW           $hw\n");
-    fclose($doc);
+        if (isset($_GET['no'])) {
+          $no = $_GET['no'];
+        }
+        if (isset($_GET['type'])) {
+          $type = $_GET['type'];
+        }
+        if (isset($_GET['value'])) {
+          $value = $_GET['value'];
+        }
+        if (isset($_GET['unit'])) {
+          $unit = $_GET['unit'];
+        }
+        if (isset($_GET['ts'])) {
+          $ts = $_GET['ts'];
+        }
+        if (isset($_GET['period'])) {
+          $period = $_GET['period'];
+        }
+        if (isset($_GET['url'])) {
+          $url = $_GET['url'];
+        }
+        if (isset($_GET['hw'])) {
+          $hw = $_GET['hw'];
+        }
 
-    //===========================================
-    // Single value
-    //===========================================
-    writeSingle($topic,$value);
 
-    // Check if any action is present for this client/topic
-    echo readActionFile($topic);
-  }
+        //===========================================
+        // Registration
+        //===========================================
+        $filename = str_replace("/","_",$topic);
+        $filename = $filename.".reg";
+        //print $filename;
+        $doc = fopen($filename, "w");
+        fwrite($doc, "$gs_ts $ts $topic $url $type $period $hw");
+        fclose($doc);
+  
+        //===========================================
+        // HTML
+        //===========================================
+
+        $fdoc = $topic.'/doc.html';
+
+        $doc = fopen($fdoc, "w");
+        fwrite($doc, "<html>");
+        fwrite($doc, "<body bgcolor=\"#9EB14A\">");
+        fwrite($doc, "TOPIC       ".$topic);
+        fwrite($doc, "<br>");
+        fwrite($doc, "NO          ".$no);
+        fwrite($doc, "<br>");
+        fwrite($doc, "TYPE        ".$type);
+        fwrite($doc, "<br>");
+        fwrite($doc, "VALUE       ".$value);
+        fwrite($doc, "<br>");
+        fwrite($doc, "UNIT        ".$unit);
+        fwrite($doc, "<br>");
+        fwrite($doc, "TS          ".$ts);
+        fwrite($doc, "<br>");
+        fwrite($doc, "PERIOD      ".$period);
+        fwrite($doc, "<br>");
+        fwrite($doc, "GS_TS       ".$gs_ts);
+        fwrite($doc, "<br>");
+        fwrite($doc, "URL         ".$url);
+        fwrite($doc, "<br>");
+        fwrite($doc, "HW          ".$hw);
+        fwrite($doc, "</body></html>");
+        fclose($doc);
+
+        //===========================================
+        // JSON
+        //===========================================
+        $fdoc = $topic.'/doc.json';
+        $doc = fopen($fdoc, "w");
+        fwrite($doc, "{\"gow\": {\n");
+        fwrite($doc, "   \"topic\":  \"$topic\",\n");
+        fwrite($doc, "   \"no\":     \"$no\",\n");
+        fwrite($doc, "   \"type\":   \"$type\",\n");
+        fwrite($doc, "   \"value\":  \"$value\",\n");
+        fwrite($doc, "   \"unit\":   \"$unit\",\n");
+        fwrite($doc, "   \"ts\":     \"$ts\",\n");
+        fwrite($doc, "   \"period\": \"$period\",\n");
+        fwrite($doc, "   \"gs_ts\":  \"$gs_ts\"\n");
+        fwrite($doc, "   \"url\":    \"$url\"\n");
+        fwrite($doc, "   \"hw\":     \"$hw\"\n");
+        fwrite($doc, "}}\n ");
+        fclose($doc);
+
+        //===========================================
+        // TXT
+        //===========================================
+        $fdoc = $topic.'/doc.txt';
+        $doc = fopen($fdoc, "w");
+        fwrite($doc,   "TOPIC        $topic\n");
+        fwrite($doc,   "NO           $no\n");
+        fwrite($doc,   "TYPE         $type\n");
+        fwrite($doc,   "VALUE        $value\n");
+        fwrite($doc,   "UNIT         $unit\n");
+        fwrite($doc,   "TS           $ts\n");
+        fwrite($doc,   "PERIOD       $period\n");
+        fwrite($doc,   "GS_TS        $gs_ts\n");
+        fwrite($doc,   "URL          $url\n");
+        fwrite($doc,   "HW           $hw\n");
+        fclose($doc);
+
+        //===========================================
+        // Single value
+        //===========================================
+        $fdoc = $topic.'/doc.single';
+        $doc = fopen($fdoc, "w");
+        fwrite($doc, "$value");
+        fclose($doc);
+
+        // Check if any action is present for this client/topic
+        echo readActionFile($topic);
+      } // data
+ } // error
+} // do
 
 // End of file
-  ?>
+?>
