5ed8b9b3f43c41c68c1b62bd4f22f5562f4b57ce
bsaxen <benny.saxen@gmail.com>
2018-11-22 12:06:28 +0100

    Update httpClient.py

--- clients/httpClient.py
+++ clients/httpClient.py
@@ -25,25 +25,29 @@ topic1 = 'test/temperature/outdoor/1'
 topic2 = 'test/temperature/outdoor/2'
 topic3 = 'test/electricity/house/0'
 #===================================================
-def http_get_value( itopic, itype, ivalue, iunit, n, iperiod, ihw ):
+def publishData( itopic, itype, ivalue, iunit, n, iperiod, ihw ):
 #===================================================
 	#url = 'http://gow.simuino.com/gowServer.php'
 	url = conf_gs_url
 	server = conf_server_name
 	data = {}
-	data['do'] = 'data'
-	data['topic'] = itopic
-	data['no'] = n
-	data['wrap'] = conf_wrap
-	data['type'] = itype
+	# meta data
+	data['do']     = 'data'
+	data['topic']  = itopic
+	data['no']     = n
+	data['wrap']   = conf_wrap
+	data['type']   = itype
+	data['ts']     = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	data['period'] = iperiod
+	data['url']    = url
+	data['hw']     = ihw
+	data['hash']   = 'nohash'
+	# payload
 	data['p1'] = 'value'
 	data['v1'] = ivalue
 	data['p2'] = 'unit'
 	data['v2'] = iunit
-	data['ts'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	data['period'] = iperiod
-	data['url'] = url
-	data['hw'] = ihw
+	
 	values = urllib.urlencode(data)
 	req = url + server + '?' + values
 	try: response = urllib2.urlopen(req)
@@ -52,7 +56,7 @@ def http_get_value( itopic, itype, ivalue, iunit, n, iperiod, ihw ):
 	the_page = response.read()
 	print 'Message to ' + itopic + ': ' + the_page
 #===================================================
-def http_get_action( itopic, iaction ):
+def placeOrder( itopic, iaction ):
 #===================================================
 	url = conf_gs_url
 	server = conf_server_name
@@ -73,11 +77,11 @@ while True:
 		n = 0
 	value = n
 	# Set an action for topic1
-	#http_get_action(topic1,'please do nothing')
+	#placeOrder(topic1,'please do nothing')
 	# Send data to topic2
-	http_get_value(topic1,'TEMPERATURE', value, 'celcius',n,conf_period,conf_hw)
-	http_get_value(topic2,'TEMPERATURE', value, 'celcius',n,conf_period,conf_hw)
-	http_get_value(topic3,'ELECTRICITY', value, 'watt',n,conf_period,conf_hw)
+	publishData(topic1,'TEMPERATURE', value, 'celcius',n,conf_period,conf_hw)
+	publishData(topic2,'TEMPERATURE', value, 'celcius',n,conf_period,conf_hw)
+	publishData(topic3,'ELECTRICITY', value, 'watt',n,conf_period,conf_hw)
 	print 'sleep ' + str(conf_period) + ' sec'
 	time.sleep(conf_period)
 
