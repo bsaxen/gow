a82bf60add8e5ce5a1350422b0a332e0b91e213c
bsaxen <benny.saxen@gmail.com>
2018-12-06 20:53:05 +0100

    Update gowMysql.py

--- applications/gowMysql.py
+++ applications/gowMysql.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python
 import MySQLdb
+import requests
+import json
+import urllib2
+import time
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="",
+
+while True:
+    scUrl = "http://gow.simuino.com/kvv32/test/temperature/0/doc.json"
+    print scUrl
+    r = urllib2.urlopen(scUrl)
+
+
+    j = json.load(r)
+    x =  j['gow']['value']
+    period =  float(j['gow']['period'])
+    print x
+
+
+
+    db = MySQLdb.connect(host="192.168.1.85",
+                     user="folke",
+                     #passwd="asdasd",
                      db="gow")
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM location")
+    sql = "INSERT INTO `temperatur1` (`id`, `value`, `ts`) VALUES (NULL," + str(x) + ", CURRENT_TIMESTAMP)"
+    print sql
+    cursor.execute(sql)
 
-# db.commit()
+    db.commit()
+    #cursor.execute("SELECT * FROM temperatur1")
+    #numrows = cursor.rowcount
+    #print numrows
+    #for x in range(0, numrows):
+    #    row = cursor.fetchone()
+    #    print row[0], "-->", row[1], "-->", row[2]
+    db.close()
 
-numrows = cursor.rowcount
-
-for x in range(0, numrows):
-    row = cursor.fetchone()
-    print row[0], "-->", row[1]
-
-db.close()
+    time.sleep(period)
