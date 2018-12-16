9ac2ebd5785891c1de272d92fa125dbdab714938
bsaxen <benny.saxen@gmail.com>
2018-12-16 15:49:38 +0100

    Update gowDeviceManager.php

--- gowDeviceManager.php
+++ gowDeviceManager.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 //=============================================
 // File.......: gowDeviceManager.php
-// Date.......: 2018-12-15
+// Date.......: 2018-12-16
 // Author.....: Benny Saxen
 // Description: Glass Of Water Platform Device Manager
 //=============================================
@@ -24,11 +25,31 @@ function addDomain ($domain)
   $fh = fopen($domain, 'w') or die("Can't add domain $domain");
 }
 //=============================================
-function restApi($rest)
+function restApi($api,$url,$topic)
 //=============================================
 {
-  echo("RestApi [$rest]<br>");
-  $res = file_get_contents($rest);
+  echo("RestApi [$api] url=$url topic=$topic<br>");
+  $call = 'http://'.$url.'/gowServer.php?do='.$api.'&topic='.$topic;
+  $res = file_get_contents($call);
+}
+
+//=============================================
+function getStatus($doc)
+//=============================================
+{
+  $url = str_replace(".html", ".json", $doc);
+  $json = file_get_contents($url);
+  $json = utf8_encode($json);
+  $res = json_decode($json, TRUE);
+  $period = $res['gow']['period'];
+  $timestamp = $res['gow']['gs_ts'];
+  $now = date_create('now')->format('Y-m-d H:i:s');
+  $diff = strtotime($now) - strtotime($timestamp);
+  if ($diff > $period) $res = $timestamp;
+  else {
+    $res = 0;
+  }
+  return ($res);
 }
 //=============================================
 // End of library
@@ -38,18 +59,46 @@ function restApi($rest)
 //=============================================
 // Back-End
 //=============================================
-
+$sel_format = $_SESSION["format"];
+$sel_url    = $_SESSION["url"];
+$sel_path   = $_SESSION["path"];
 
 if (isset($_GET['do'])) {
+
   $do = $_GET['do'];
+
   if($do == 'select')
   {
-    $sel_doc = $_GET['sel_doc'];
+    if (isset($_GET['sel_url']))
+    {
+      $sel_url = $_GET['sel_url'];
+      $_SESSION["url"] = $sel_url;
+    }
+    if (isset($_GET['sel_path']))
+    {
+      $sel_path = $_GET['sel_path'];
+      $_SESSION["path"]   = $sel_path;
+    }
+    if (isset($_GET['sel_format']))
+    {
+      $sel_format = $_GET['sel_format'];
+      $_SESSION["format"] = $sel_format;
+    }
   }
-  if($do == 'rest_api')
+
+  if($do == 'delete')
   {
-    $rest = $_GET['rest'];
-    restApi($rest);
+    $domain   = $_GET['domain'];
+    $fn = $domain.'.domain';
+    if (file_exists($fn)) unlink($fn);
+  }
+
+  if($do == 'rest')
+  {
+    $api   = $_GET['api'];
+    $url   = $_GET['url'];
+    $topic = $_GET['topic'];
+    restApi($api,$url,$topic);
   }
 }
 
@@ -78,9 +127,19 @@ echo "<html>
       <title>GOW Device Manager</title>
    </head>
    <body> ";
-   echo ("<a href=#>refresh</a><br>");
 
-   echo ("<iframe src=$sel_doc></iframe>");
+echo("<h1>GOW Device Manager 2018-12-16</h1>");
+echo("url=$sel_url topic=$sel_path format=$sel_format<br>");
+   //echo ("<a href=#>refresh</a><br>");
+
+   $doc = 'http://'.$sel_url.'/'.$sel_path.'/doc.'.$sel_format;
+   echo ("<iframe src=$doc width=\"400\" height=\"300\"></iframe>");
+   if($sel_url)
+   {
+     echo ("<br><a href=gowDeviceManager.php?do=select&sel_format=html>html</a> ");
+     echo ("<a href=gowDeviceManager.php?do=select&sel_format=json>json</a> ");
+     echo ("<a href=gowDeviceManager.php?do=select&sel_format=txt>txt</a> ");
+   }
 
    $do = 'ls *.domain > domain.list';
    system($do);
@@ -89,43 +148,74 @@ echo "<html>
    {
      echo "<br><br>
      <table border=1>";
+     echo "
+     <form action=\"#\" method=\"post\">
+       <input type=\"hidden\" name=\"do\" value=\"add_domain\">
+       <tr><td>Add Domain</td><td> <input type=\"text\" name=\"domain\" size=40></td>
+       <td><input type= \"submit\" value=\"Add Domain\"></td></tr>
+     </form>
+     ";
+
+     echo "<tr bgcolor=\"#FFC300\"><td>Domain</td><td>Status/Topic</td><td>Edit</td></tr>";
 
      while(!feof($file))
      {
        $line = fgets($file);
-
+       //echo "<tr><td>$line</td><td>benny</td></tr>";
        if (strlen($line) > 2)
        {
            $line = trim($line);
            $url = str_replace(".domain", "", $line);
            $request = 'http://'.$url."/gowServer.php?do=list";
            //echo $request;
-           $res = file_get_contents($request);
+           $ctx = stream_context_create(array('http'=>
+            array(
+              'timeout' => 2,  //2 Seconds
+                )
+              ));
+           $res = file_get_contents($request,false,$ctx);
+           //echo "<tr><td>$res</td><td>b1</td></tr>";
+           //echo "<tr><td>$url</td><td>b2</td></tr>";
+           if ($res === false) {
+             echo "<tr><td>$url</td><td bgcolor=\"red\">NO CONNECTION</td>";
+           }
+           else {
+             echo "<tr><td>$url</td><td bgcolor=\"#DAF7A6\">CONNECTED</td>";
+           }
+           echo "<td><a href=gowDeviceManager.php?do=delete&domain=$url>delete</a></td></tr>";
 
-           echo "<tr><td>$url</td><td></td></tr>";
            $data = explode(":",$res);
            $num = count($data);
 
            for ($ii = 0; $ii < $num; $ii++)
            {
-             $tmp = str_replace(".reg", "", $data[$ii]);
-             if (strlen($tmp) > 2)
+             $id = str_replace(".reg", "", $data[$ii]);
+             if (strlen($id) > 2)
              {
-               $topic = explode("_",$tmp);
+               $topic = explode("_",$id);
                $topic_num = count($topic);
                //$link = 'http://'.$url;
-               $link = "";
-               for ($jj=0;$jj<$topic_num;$jj++)
+               $link = $topic[0];
+               for ($jj=1;$jj<$topic_num;$jj++)
                   $link = $link."/$topic[$jj]";
-               $doc = 'http://'.$url.$link.'/doc.html';
-               echo "<tr><td></td><td><a href=gowDeviceManager.php?do=select&sel_doc=$doc>$link</a></td>";
+               $doc = 'http://'.$url.'/'.$link.'/doc.html';
+               $status = getStatus($doc);
+               if ($status == 0)
+               {
+                 echo "<tr><td bgcolor=\"#DAF7A6\">ON-LINE</td>";
+               }
+               else {
+                 echo "<tr><td bgcolor=\"yellow\">OFF-LINE</td>";
+               }
+               echo "<td><a href=gowDeviceManager.php?do=select&sel_url=$url&sel_path=$link>$link</a></td>";
                $rest = 'http://'.$url.'?do=delete&topic='.$link;
                //echo "<tr><td></td><td><a href=gowDeviceManager.php?do=select&sel_doc=$doc>$link</a></td>";
-               echo "<td><a href=gowDeviceManager.php?do=rest_api&rest=$rest>delete</a></td></tr>";
+               echo "<td><a href=gowDeviceManager.php?do=rest&api=delete&url=$url&topic=$link>remove</a></td></tr>";
              }
            }
        }
      }
+
      echo("</table>");
 
 
@@ -135,7 +225,7 @@ echo "<html>
 
 // Add domain - Remove domain -
 // List domains
-    echo "<br><br>
+/*    echo "<br><br>
     <table border=1>
     <form action=\"#\" method=\"post\">
       <input type=\"hidden\" name=\"do\" value=\"add_domain\">
@@ -143,7 +233,7 @@ echo "<html>
       <tr><td><input type= \"submit\" value=\"Add Domain\"></td><td></td></tr>
     </form></table>
     ";
-
+*/
 
 echo "</body></html>";
 // End of file
