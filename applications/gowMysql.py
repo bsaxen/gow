11159ce5a736e31e173b8b571d620f80a0a2ae91
bsaxen <benny.saxen@gmail.com>
2019-01-01 16:28:50 +0100

    Update gowMysql.py

--- applications/gowMysql.py
+++ applications/gowMysql.py
@@ -19,6 +19,30 @@ cDbHost     = '192.168.1.85'
 cDbName     = 'gow'
 cDbUser     = 'folke'
 cDbPassword = 'something'
+#===================================================
+def readConfiguration():
+	try:
+		fh = open('configuration.txt', 'r') 
+		for line in fh: 
+			print line
+			word = line.split()
+			if word[0] == 'host':
+				cDbHost      = word[1]
+			if word[0] == 'database':
+				cDbName = word[1]
+			if word[0] == 'user':
+				cDbUser      = word[1]
+			if word[0] == 'pswd:
+				cDbPassword  = word[1]
+		fh.close()
+	except:
+		fh = open('configuration.txt', 'w')
+		fh.write('host       192.168.1.85\n')
+		fh.write('database   gow\n')
+		fh.write('user       folke\n')
+		fh.write('pswd       hm\n')
+		fh.close()
+	return
 #=============================================
 def gowReadJsonMeta(url,par):
 #=============================================
@@ -45,9 +69,15 @@ def gowMysqlInsert(xTable,xPar,xValue):
     cursor.execute(sql)
     db.commit()
     db.close()
+    
+#=============================================
+# setup
+#=============================================    
+readConfiguration()
+#=============================================
+# loop
 #=============================================
 while True:
-#=============================================
     url = "http://gow.simuino.com/kvv32/test/temperature/0/device.json"
     
     period = float(gowReadJsonMeta(url,'period'))
