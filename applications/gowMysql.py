663b71e8477a247d4f4ea291ee703715161c7d1b
bsaxen <benny.saxen@gmail.com>
2019-01-05 22:20:18 +0100

    Update gowMysql.py

--- applications/gowMysql.py
+++ applications/gowMysql.py
@@ -3,7 +3,7 @@
 # File.......: gowMysql.py
 # Date.......: 2019-01-05
 # Author.....: Benny Saxen
-# Description: 
+# Description:
 #=============================================
 # Libraries
 #=============================================
@@ -12,62 +12,80 @@ import requests
 import json
 import urllib2
 import time
+import sys
+
+if len (sys.argv) != 2 :
+    print "Usage: python gowMysql.py your-configuration.txt"
+    sys.exit (1)
+
+config_file = sys.argv[1]
+print "Configuration used: " + config_file
+
 #=============================================
 # Configuration
 #=============================================
 cUrl        = 'gow.simuino.com'
-cNtop	    = 1
-cTopic1     = 'kvv32/test/temperature/0'
+cTopic      = 'kvv32/test/temperature/0'
+cParam      = 'temp1'
 cDbHost     = '192.168.1.85'
 cDbName     = 'gow'
 cDbUser     = 'folke'
 cDbPassword = 'something'
-cDbTableName1 = 'something'
-cDbTableName2 = 'something'
+cDbTableName = 'something'
 #===================================================
 def readConfiguration():
-	try:
-		fh = open('configuration.txt', 'r') 
-		for line in fh: 
-			print line
-			word = line.split()
-			if word[0] == 'url':
-				cUrl    = word[1]
-			if word[0] == 'url':
-				cNtop   = word[1]
-			if word[0] == 'topic1':
-				cTopic1    = word[1]
-			if word[0] == 'topic2':
-				cTopic2    = word[1]
-			if word[0] == 'host':
-				cDbHost      = word[1]
-			if word[0] == 'database':
-				cDbName = word[1]
-			if word[0] == 'user':
-				cDbUser      = word[1]
-			if word[0] == 'pswd':
-				cDbPassword  = word[1]
-
-			if word[0] == 'tablename1':
-				cDbTableName1  = word[1]
-				print cDbTableName1 + "benny"
-			if word[0] == 'tablename2':
-				cDbTableName2  = word[1]
-		fh.close()
-	except:
-		fh = open('configuration.txt', 'w')
-		fh.write('url        gow.simuino.com\n')
-		fh.write('ntop       1\n')
-		fh.write('topic1     kvv32/test/temperature/0\n')
-		fh.write('topic2     kvv32/test/temperature/1\n')
-		fh.write('host       192.168.1.85\n')
-		fh.write('database   gow\n')
-		fh.write('user       folke\n')
-		fh.write('pswd       hm\n')
-		fh.write('tablename1 hm\n')
-		fh.write('tablename2 hm\n')
-		fh.close()
-	return
+    global cUrl
+    global cTopic
+    global cParam
+    global cDbHost
+    global cDbName
+    global cDbUser
+    global cDbPassword
+    global cDbTableName
+    try:
+        fh = open(config_file, 'r')
+        for line in fh:
+            #print line
+            word = line.split()
+            if word[0] == 'url':
+                cUrl    = word[1]
+                print cUrl
+            if word[0] == 'topic':
+                cTopic  = word[1]
+                print cTopic
+            if word[0] == 'param':
+                cParam  = word[1]
+                print cParam
+            if word[0] == 'host':
+                cDbHost = word[1]
+                print cDbHost
+            if word[0] == 'database':
+                cDbName = word[1]
+                print cDbName
+            if word[0] == 'user':
+                cDbUser = word[1]
+                print cDbUser
+            if word[0] == 'pswd':
+                cDbPassword  = word[1]
+                print cDbPassword
+            if word[0] == 'tablename':
+                cDbTableName  = word[1]
+                print cDbTableName
+        fh.close()
+    except:
+        print "Configuration file not found - configuration.txt template created"
+        fh = open('configuration.txt', 'w')
+        fh.write('url        gow.simuino.com\n')
+        fh.write('topic      kvv32/test/temperature/0\n')
+        fh.write('param      temp1\n')
+        fh.write('host       192.168.1.85\n')
+        fh.write('database   gow\n')
+        fh.write('user       folke\n')
+        fh.write('pswd       hm\n')
+        fh.write('tablename  mytable\n')
+        fh.close()
+        sys.exit (1)
+    return
 #=============================================
 def gowReadJsonMeta(url,par):
 #=============================================
@@ -85,37 +103,40 @@ def gowReadJsonPayload(url,par):
 #=============================================
 def gowMysqlInsert(xTable,xPar,xValue):
 #=============================================
-    db = MySQLdb.connect(host=cDbHost,
-                     user=cDbUser,
-                     #passwd=cDbPassword,
-                     db=cDbName)
+    global cDbHost
+    global cDbName
+    global cDbUser
+    global cDbPassword
+    print xTable + xPar + str(xValue)
+    db = MySQLdb.connect(host=cDbHost,user=cDbUser,db=cDbName)
+    print "a1"
     cursor = db.cursor()
+    print "a2"
+    sql = "CREATE TABLE IF NOT EXISTS " + xTable + " (id int(11) NOT NULL AUTO_INCREMENT,value float,ts timestamp, PRIMARY KEY (id))"
+    cursor.execute(sql)
+    print "a3"
     sql = "INSERT INTO "+ xTable + " (`id`, " + xPar + ", `ts`) VALUES (NULL," + str(xValue) + ", CURRENT_TIMESTAMP)"
     cursor.execute(sql)
+    print "a4"
     db.commit()
+    print "a5"
     db.close()
-    
+
 #=============================================
 # setup
-#=============================================    
+#=============================================
 readConfiguration()
 #=============================================
 # loop
 #=============================================
 while True:
-
-	print "t1 " + cDbTableName1
-	if cNtop > 0:
-		url = 'http://' + cUrl + '/' + cTopic1 + '/device.json'
-   		period = float(gowReadJsonMeta(url,'period'))
-    		x      = float(gowReadJsonPayload(url,'temp1'))
-    		gowMysqlInsert(cDbTableName1,'value',x)
-
-	if cNtop > 1:
-   		url = 'http://' + cUrl + '/' + cTopic2 + '/device.json'
-   		period = float(gowReadJsonMeta(url,'period'))
-    		x      = float(gowReadJsonPayload(url,'temp2'))
-    		gowMysqlInsert(cDbTableName2,'value',x)
-		
-    	print "sleep " + str(period)
-    	time.sleep(period)
+    print cTopic
+    url = 'http://' + cUrl + '/' + cTopic + '/device.json'
+    print url
+    period = float(gowReadJsonMeta(url,'period'))
+    print period
+    x      = float(gowReadJsonPayload(url,cParam))
+    print x
+    gowMysqlInsert(cDbTableName,'value',x)
+    print "sleep " + str(period)
+    time.sleep(period)
