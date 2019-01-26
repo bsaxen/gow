26b48b824bcc725f62779d03ce3b3683957bed5b
bsaxen <benny.saxen@gmail.com>
2019-01-26 20:12:54 +0100

    Update gowServer.php

--- gowServer.php
+++ gowServer.php
@@ -261,7 +261,7 @@ if (isset($_GET['do']))
           $hw = $_GET['hw'];
         }
         if (isset($_GET['ssid'])) {
-          $hw = $_GET['ssid'];
+          $ssid = $_GET['ssid'];
         }
         if (isset($_GET['message'])) {
           $message = $_GET['message'];
