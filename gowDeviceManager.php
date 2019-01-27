4c3ad72acca49214ed675cfad5f5c702e23da021
bsaxen <benny.saxen@gmail.com>
2019-01-27 08:19:20 +0100

    Update gowDeviceManager.php

--- gowDeviceManager.php
+++ gowDeviceManager.php
@@ -250,7 +250,7 @@ if ($form_send == 1)
                echo "<td><a href=gowDeviceManager.php?do=select&sel_url=$url&sel_path=$link>$link</a></td>";
                $rest = 'http://'.$url.'?do=delete&topic='.$link;
                //echo "<tr><td></td><td><a href=gowDeviceManager.php?do=select&sel_doc=$doc>$link</a></td>";
-               if ($g_message == 2 and $status == 0 ) {
+               if ($g_message == 2) {
                   echo "<td><a href=gowDeviceManager.php?do=form&api=action&url=$url&topic=$link>send</a></td>";
               }
               else {
