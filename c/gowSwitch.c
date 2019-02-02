8b048a48fa3f06c18fa630cd7d6c3f965fbfc35e
bsaxen <benny.saxen@gmail.com>
2019-02-02 10:54:55 +0100

    Update gowSwitch.c

--- c/gowSwitch.c
+++ c/gowSwitch.c
@@ -1,31 +1,17 @@
 //=============================================
 // File.......: gowSwitch.c
-// Date.......: 2019-02-01
+// Date.......: 2019-02-02
 // Author.....: Benny Saxen
 // Description:
 //=============================================
 // Configuration
 //=============================================
-#include "gowLib.c"
+//#include "gowLib.c"
 
 struct Configuration c1;
 struct Data d1;
 int wifi_ss = 0;
 
-
-c1.conf_topic = "test/topic/here/0";
-c1.conf_period  = 10;
-c1.conf_wrap    = 999999;
-c1.conf_action  = 1;
-
-c1.conf_tags = "tag1,tag2,tag3";
-c1.conf_desc = "your_description";
-c1.conf_platform = "esp8266";
-c1.ssid       = "my_ssid";
-c1.password   = "my passw";
-c1.host       = "192.168.1.242";
-c1.streamId   = "....................";
-c1.privateKey = "....................";
 //=============================================
 
 #define FAN_PIN 5  // D1 pin on NodeMCU 1.0
@@ -34,10 +20,23 @@ int g_status = 0;
 //=============================================
 void setup() {
 //=============================================
+  c1.conf_topic = "benny/saxen/2";
+  c1.conf_period  = 10;
+  c1.conf_wrap    = 999999;
+  c1.conf_action  = 1;
+
+  c1.conf_tags = "tag1,tag2,tag3";
+  c1.conf_desc = "your_description";
+  c1.conf_platform = "esp8266";
+  c1.conf_ssid       = "bridge";
+  c1.conf_password   = "qweqwe";
+  c1.conf_host       = "gow.qwe.com";
+  c1.conf_streamId   = "....................";
+  c1.conf_privateKey = "....................";
   Serial.begin(9600);
   delay(100);
 
-  wifiBegin(c1);
+  lib_wifiBegin(c1);
 
   pinMode(FAN_PIN, OUTPUT);
   digitalWrite(FAN_PIN,HIGH);
@@ -52,16 +51,18 @@ void setup() {
 void loop()
 //=============================================
 {
-  char msg[100];
+  String msg;
+  char buf[100];
 
-  delay(c1.conf_period*1000);
+  Serial.println("Start...");
 
   ++d1.counter;
   if (d1.counter > c1.conf_wrap) d1.counter = 1;
-
-  lib_buildUrlStatic(c1, stat_url);
-  lib_buildUrlDynamic(c1, d1, dyn_url);
-
+  Serial.println(d1.counter);
+  String stat_url = lib_buildUrlStatic(c1);
+  String dyn_url = lib_buildUrlDynamic(c1, d1);
+  Serial.println(stat_url);
+  Serial.println(dyn_url);
   String cur_url = " ";
   if (d1.counter%100 == 0)
   {
@@ -71,14 +72,16 @@ void loop()
   {
     cur_url = dyn_url;
   }
-
+  Serial.println(cur_url);
   msg = lib_wifiConnectandSend(c1, cur_url);
-
+  Serial.println(msg);
   int res = lib_decode_ON_OFF(msg);
 
   if (res == 1) digitalWrite(FAN_PIN,LOW);
   if (res == 2) digitalWrite(FAN_PIN,HIGH);
   g_status = res;
+
+  delay(c1.conf_period*1000);
 }
 //=============================================
 // End of File
