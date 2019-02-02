6b02b466c118ec7d29f48ca6c2feee203e46814c
bsaxen <benny.saxen@gmail.com>
2019-02-02 11:15:37 +0100

    Update gowTemperatureSensor.c

--- c/gowTemperatureSensor.c
+++ c/gowTemperatureSensor.c
@@ -1,6 +1,6 @@
 //=============================================
 // File.......: gowTemperatureSensor.c
-// Date.......: 2019-02-01
+// Date.......: 2019-02-02
 // Author.....: Benny Saxen
 // Description: Signal from D1 pin.
 // 4.7kOhm between signal and Vcc
@@ -8,26 +8,13 @@
 //=============================================
 // Configuration
 //=============================================
-#include "gowLib.c"
+//#include "gowLib.c"
 
 struct Configuration c1;
 struct Data d1;
 int wifi_ss = 0;
 
 
-c1.conf_topic = "test/topic/here/0";
-c1.conf_period  = 10;
-c1.conf_wrap    = 999999;
-c1.conf_action  = 1;
-
-c1.conf_tags = "tag1,tag2,tag3";
-c1.conf_desc = "your_description";
-c1.conf_platform = "esp8266";
-c1.ssid       = "my_ssid";
-c1.password   = "my passw";
-c1.host       = "192.168.1.242";
-c1.streamId   = "....................";
-c1.privateKey = "....................";
 
 //=============================================
 
@@ -67,6 +54,20 @@ void SetUpTemperatureSensors()
 void setup()
 //=============================================
 {
+ 
+  c1.conf_topic = "test/topic/here/0";
+  c1.conf_period  = 10;
+  c1.conf_wrap    = 999999;
+  c1.conf_action  = 1;
+
+  c1.conf_tags = "tag1,tag2,tag3";
+  c1.conf_desc = "your_description";
+  c1.conf_platform = "esp8266";
+  c1.conf_ssid       = "my_ssid";
+  c1.conf_password   = "my passw";
+  c1.conf_host       = "192.168.1.242";
+  c1.conf_streamId   = "....................";
+  c1.conf_privateKey = "....................";
   Serial.begin(9600);
   delay(10);
 
@@ -87,13 +88,10 @@ void loop()
   char order[10];
   char buf[100];
 
-  char msg[100];
   delay(c1.conf_period*1000);
   ++d1.counter;
 
   if (d1.counter > c1.conf_wrap) d1.counter = 1;
-
-
 
   //Retrieve one or more temperature values
   sensor.requestTemperatures();
@@ -108,11 +106,8 @@ void loop()
       }
   }
 
-
-
-
-  lib_buildUrlStatic(c1, stat_url);
-  lib_buildUrlDynamic(c1, d1, dyn_url);
+  String stat_url = lib_buildUrlStatic(c1);
+  String dyn_url = lib_buildUrlDynamic(c1, d1);
 
   String cur_url = " ";
   if (d1.counter%100 == 0)
@@ -138,7 +133,7 @@ void loop()
     cur_url += "}";
   }
 
-  msg = lib_wifiConnectandSend(c1, cur_url);
+  String msg = lib_wifiConnectandSend(c1, cur_url);
 
 }
 //=============================================
