5cea7015e86d2e4e2ede1f2da2646261964b661e
bsaxen <benny.saxen@gmail.com>
2019-02-02 11:12:56 +0100

    Update gowStepperMotor.c

--- c/gowStepperMotor.c
+++ c/gowStepperMotor.c
@@ -1,6 +1,6 @@
 //=============================================
 // File.......: gpwStepperMotor.c
-// Date.......: 2019-02-01
+// Date.......: 2019-02-02
 // Author.....: Benny Saxen
 // Description:
 // Message Api:
@@ -17,20 +17,6 @@ struct Configuration c1;
 struct Data d1;
 int wifi_ss = 0;
 
-
-c1.conf_topic = "test/topic/here/0";
-c1.conf_period  = 10;
-c1.conf_wrap    = 999999;
-c1.conf_action  = 1;
-
-c1.conf_tags = "tag1,tag2,tag3";
-c1.conf_desc = "your_description";
-c1.conf_platform = "esp8266";
-c1.ssid       = "my_ssid";
-c1.password   = "my passw";
-c1.host       = "192.168.1.242";
-c1.streamId   = "....................";
-c1.privateKey = "....................";
 //================================================
 // Globals
 //================================================
@@ -200,6 +186,21 @@ asm volatile ("  jmp 0");
 //================================================
 void setup(void){
 //================================================
+    
+   c1.conf_topic = "test/topic/here/0";
+   c1.conf_period  = 10;
+   c1.conf_wrap    = 999999;
+   c1.conf_action  = 1;
+
+   c1.conf_tags = "tag1,tag2,tag3";
+   c1.conf_desc = "your_description";
+   c1.conf_platform = "esp8266";
+   c1.conf_ssid       = "my_ssid";
+   c1.conf_password   = "my passw";
+   c1.conf_host       = "192.168.1.242";
+   c1.conf_streamId   = "....................";
+   c1.conf_privateKey = "....................";
+  
    Serial.begin(9600);
     //Initialize
     pinMode(DIR,OUTPUT);
@@ -225,15 +226,14 @@ void loop(void){
 //================================================
 
   int move = 0;
-  char msg[100];
   
   delay(c1.conf_period*1000);
   ++d1.counter;
 
   if (d1.counter > c1.conf_wrap) d1.counter = 1;
 
-  lib_buildUrlStatic(c1, stat_url);
-  lib_buildUrlDynamic(c1, d1, dyn_url);
+  String stat_url = lib_buildUrlStatic(c1);
+  String dyn_url = lib_buildUrlDynamic(c1, d1);
 
 
   String cur_url = " ";
@@ -254,9 +254,9 @@ void loop(void){
     cur_url += "}";
   }
 
-  msg = lib_wifiConnectandSend(c1, cur_url);
+  String msg = lib_wifiConnectandSend(c1, cur_url);
 
-  res = lib_decode_STEPPER(msg);
+  int res = lib_decode_STEPPER(msg);
 
   move = 0;
   if (res < 100 && res > 0)
