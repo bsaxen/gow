f85fe2690ce85f50e47364868fb356fbe938d12b
bsaxen <benny.saxen@gmail.com>
2019-02-03 09:27:56 +0100

    Update gowSwitch.c

--- c/gowSwitch.c
+++ c/gowSwitch.c
@@ -1,6 +1,6 @@
 //=============================================
 // File.......: gowSwitch.c
-// Date.......: 2019-02-02
+// Date.......: 2019-02-03
 // Author.....: Benny Saxen
 // Description:
 //=============================================
@@ -15,24 +15,26 @@ String dyn_url;
 //=============================================
 
 #define FAN_PIN 5  // D1 pin on NodeMCU 1.0
-int g_status = 0;
+int g_status = 1;
 
 //=============================================
 void setup() {
 //=============================================
-  c1.conf_topic = "benny/saxen/2";
-  c1.conf_period  = 10;
+  c1.conf_topic = "nytomta/gow/qwe/0";
+  c1.conf_period  = 60;
   c1.conf_wrap    = 999999;
-  c1.conf_action  = 1;
+  c1.conf_action  = 2;
 
   c1.conf_tags = "tag1,tag2,tag3";
-  c1.conf_desc = "your_description";
+  c1.conf_desc = "floor_fan";
   c1.conf_platform = "esp8266";
-  c1.conf_ssid       = "bridge";
+  c1.conf_ssid       = "ASUS-hus";
   c1.conf_password   = "qweqwe";
   c1.conf_host       = "gow.qwe.com";
   c1.conf_streamId   = "....................";
   c1.conf_privateKey = "....................";
+
+  d1.fail = 0;
   Serial.begin(9600);
   delay(100);
 
@@ -45,43 +47,36 @@ void setup() {
   delay(2000);
   digitalWrite(FAN_PIN,HIGH);
   g_status = 1;
-  
+
   stat_url = lib_buildUrlStatic(c1);
-  String dont_care = lib_wifiConnectandSend(c1, stat_url);
+  String dont_care = lib_wifiConnectandSend(c1,d1, stat_url);
 }
 
 //=============================================
 void loop()
 //=============================================
 {
-  Serial.println("Start...");
-
   ++d1.counter;
   if (d1.counter > c1.conf_wrap) d1.counter = 1;
   d1.rssi = WiFi.RSSI();
-  
   dyn_url = lib_buildUrlDynamic(c1, d1);
   dyn_url += "&payload=";
   dyn_url += "{";
   dyn_url += "\"status\":\"";
   dyn_url += g_status;
   dyn_url += "\"}";
-  
-  String msg = lib_wifiConnectandSend(c1, cur_url);
-  Serial.println(msg);
+  String msg = lib_wifiConnectandSend(c1,d1, dyn_url);
   int res = lib_decode_ON_OFF(msg);
-
   if (res == 1) 
-  {
-    digitalWrite(FAN_PIN,LOW);
-    g_status = res;
-  }
-  if (res == 2) 
   {
     digitalWrite(FAN_PIN,HIGH);
     g_status = res;
   }
-  
+  if (res == 2) 
+  {
+    digitalWrite(FAN_PIN,LOW);
+    g_status = res;
+  }
   delay(c1.conf_period*1000);
 }
 //=============================================
