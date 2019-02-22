f1d24bedbffc7f1cf57958776d0fdb4f1d4cfe02
bsaxen <benny.saxen@gmail.com>
2019-02-22 11:16:10 +0100

    Update gowNilm.c

--- c/gowNilm.c
+++ c/gowNilm.c
@@ -1,8 +1,9 @@
 //=============================================
 // File.......: gowNilm.c
-// Date.......: 2019-02-01
+// Date.......: 2019-02-22
 // Author.....: Benny Saxen
 // Description: nilm - electricity
+// Sensor pin.: D1 NodeMcu
 //=============================================
 // Configuration
 //=============================================
@@ -12,8 +13,8 @@ struct Configuration c1;
 struct Data d1;
 //=============================================
 
-const byte interrupt_pin = 5;
-const byte led_pin       = 4;
+const byte interrupt_pin = 5; // D1 NodeMcu
+const byte led_pin       = 4; // D2 NodeMcu
 int timeToCheckStatus    = 0;
 unsigned long t1,t2,dt,ttemp;
 float elpow               = 0.0;
@@ -44,9 +45,9 @@ void ICACHE_RAM_ATTR measure(){
 void setup(){
 //===============================================================
     c1.conf_topic = "test/topic/here/0";
-    c1.conf_period  = 10;
+    c1.conf_period  = 5;
     c1.conf_wrap    = 999999;
-    c1.conf_action  = 1;
+    c1.conf_action  = 2;
 
     c1.conf_tags = "tag1,tag2,tag3";
     c1.conf_desc = "your_description";
@@ -65,37 +66,28 @@ void setup(){
     attachInterrupt(interrupt_pin, measure, FALLING);
     lib_wifiBegin(c1);
     d1.counter = 0;
+    String stat_url = lib_buildUrlStatic(c1);
 }
 //=============================================
 void loop()
 //=============================================
 {
-  ++d1.counter;
-  if (d1.counter > c1.conf_wrap) d1.counter = 1;
-  d1.rssi = WiFi.RSSI();
-  String stat_url = lib_buildUrlStatic(c1);
-  String dyn_url = lib_buildUrlDynamic(c1, d1);
-
-  String cur_url = " ";
-  if (d1.counter%100 == 0)
-  {
-    cur_url = stat_url;
-  }
-  else
-  {
-    cur_url = dyn_url;
+    ++d1.counter;
+    if (d1.counter > c1.conf_wrap) d1.counter = 1;
+    d1.rssi = WiFi.RSSI();
+ 
+    String dyn_url = lib_buildUrlDynamic(c1, d1);
     // Add payload
-    cur_url += "&payload=";
-    cur_url += "{";
-        cur_url += "\"elpow";
-        cur_url += "\":\"";
-        cur_url += elpow;
-        cur_url += "\"";
-    cur_url += "}";
-  }
-
-  String msg = lib_wifiConnectandSend(c1, cur_url);
-  delay(c1.conf_period*1000);
+    dyn_url += "&payload=";
+    dyn_url += "{";
+    dyn_url += "\"elpow";
+    dyn_url += "\":\"";
+    dyn_url += elpow;
+    dyn_url += "\"";
+    dyn_url += "}";
+  
+    String msg = lib_wifiConnectandSend(c1, dyn_url);
+    delay(c1.conf_period*1000);
 }
 //=============================================
 // End of File
