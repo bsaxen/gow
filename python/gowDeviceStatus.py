08ba1f748735327fe39c1f1eb8b7aa2ac64d1cff
bsaxen <benny.saxen@gmail.com>
2019-02-25 20:36:44 +0100

    Update gowDeviceStatus.py

--- python/gowDeviceStatus.py
+++ python/gowDeviceStatus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #=============================================
 # File.......: gowDeviceStatus.py
-# Date.......: 2019-02-23
+# Date.......: 2019-02-25
 # Author.....: Benny Saxen
 # Description:
 #=============================================
@@ -21,21 +21,21 @@ status   = []
 
 confile = 'gowdevicestatus.conf'
 lib_readConfiguration(confile,co)
-lib_gowPublishStatic(co)
+lib_gowPublishMyStatic(co)
 
 domain = co.mydomain
 
-lib_gowPublishDynamic(co,md,'{}')
+lib_gowPublishMyDynamic(co,md,'{}')
 
-device_list = lib_listDomainDevices(domain)
+device_list = lib_apiListDomainDevices(domain)
 no_devices = len(device_list) - 1
 print "Number of devices: " + str(no_devices)
 
 max_period = 0
 for num in range(0,no_devices):
-    url_static  = lib_buildUrl(domain,device_list[num],'static')
-    url_dynamic = lib_buildUrl(domain,device_list[num],'dynamic')
-    url_payload = lib_buildUrl(domain,device_list[num],'payload')
+    url_static  = lib_buildAnyUrl(domain,device_list[num],'static')
+    url_dynamic = lib_buildAnyUrl(domain,device_list[num],'dynamic')
+    url_payload = lib_buildAnyUrl(domain,device_list[num],'payload')
 
 
     period = float(lib_readJsonMeta(url_static,'period'))
@@ -46,10 +46,10 @@ for num in range(0,no_devices):
     print desc
     schedule.append(period)
     work.append(period)
-    no = float(lib_readJsonMeta(url_dynamic,'no'))
-    running.append(no)
+    counter = float(lib_readJsonMeta(url_dynamic,'counter'))
+    running.append(counter)
     status.append(0)
-    print no
+    print counter
 #=============================================
 # loop
 #=============================================
@@ -74,14 +74,14 @@ while True:
         payload += '\"' + device_list[no_devices-1] + '\" :' + '\"' + str(status[no_devices-1]) +'\"'
         payload += '}'
         print payload
-        lib_gowPublishDynamic(co,md,payload)
+        lib_gowPublishMyDynamic(co,md,payload)
     for num in range(0,no_devices):
         work[num] -= 1
         print str(num) + " www " + str(work[num])
         if work[num] == 0:
-            url_static  = lib_buildUrl(domain,device_list[num],'static')
-            url_dynamic = lib_buildUrl(domain,device_list[num],'dynamic')
-            url_payload = lib_buildUrl(domain,device_list[num],'payload')
+            url_static  = lib_buildAnyUrl(domain,device_list[num],'static')
+            url_dynamic = lib_buildAnyUrl(domain,device_list[num],'dynamic')
+            url_payload = lib_buildAnyUrl(domain,device_list[num],'payload')
             work[num] = schedule[num]
 
             period = float(lib_readJsonMeta(url_static,'period'))
@@ -92,24 +92,24 @@ while True:
             print desc
             schedule[num] = period
 
-            no = float(lib_readJsonMeta(url_dynamic,'no'))
-            print no
-            delta_no = no - running[num]
+            counter = float(lib_readJsonMeta(url_dynamic,'counter'))
+            print counter
+            delta_counter = counter - running[num]
             ok = 0
-            if delta_no == 1:
-                print "Correct data: " + str(delta_no)
+            if delta_counter == 1:
+                print "Correct data: " + str(delta_counter)
                 ok = 0
-            if delta_no > 1:
-                print "Missing data: " + str(delta_no)
+            if delta_counter > 1:
+                print "Missing data: " + str(delta_counter)
                 ok = 1
-            if delta_no == 0:
-                print "No update of data: " + str(delta_no)
+            if delta_counter == 0:
+                print "No update of data: " + str(delta_counter)
                 ok = 2
-            if delta_no < 0:
-                print "Wrap around of data: " + str(delta_no)
+            if delta_counter < 0:
+                print "Wrap around of data: " + str(delta_counter)
                 ok = 3
 
-            running[num] = no
+            running[num] = counter
             status[num] = ok
 
 #===================================================
