dcbfd1608c321507b593272cd4f60afb913923a6
bsaxen <benny.saxen@gmail.com>
2019-02-25 20:43:48 +0100

    Update gowHttpClient.py

--- python/gowHttpClient.py
+++ python/gowHttpClient.py
@@ -1,7 +1,7 @@
 # =============================================
 # File: gowHttpClient.py
 # Author: Benny Saxen
-# Date: 2019-02-24
+# Date: 2019-02-25
 # Description:
 # =============================================
 from gowLib import *
@@ -10,19 +10,19 @@ from gowLib import *
 #===================================================
 confile = "gowhttpclient.conf"
 lib_readConfiguration(confile,co)
-lib_gowPublishStatic(co)
+lib_gowPublishMyStatic(co)
 #===================================================
 # Loop
 #===================================================
 while True:
-    lib_gowIncreaseCounter(co,md)
+    lib_gowIncreaseMyCounter(co,md)
 
 	payload = '{}'
-	msg = lib_gowPublishDynamic(co,md,payload)
+	msg = lib_gowPublishMyDynamic(co,md,payload)
 	lib_common_action(co,msg)
 
 	message = 'counter:' + str(md.mycounter)
-	lib_gowPublishLog(co, message)
+	lib_gowPublishMyLog(co, message)
 
 	print "sleep: " + str(co.myperiod) + " triggered: " + str(co.mycounter)
 	time.sleep(float(co.myperiod))
